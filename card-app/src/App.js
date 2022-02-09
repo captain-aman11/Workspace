dd17e0fc015aee1680806c36156017830d6ed1b5
captain-aman11 <kmraman11011@gmail.com>
2022-02-09 11:36:35 +0000

    SI and SU UI Commit

--- card-app/src/App.js
+++ card-app/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import SignIn from './components/signIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
+import React from "react";
+import SignIn from "./components/signIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
-  return <div>
-    {/* <SignIn /> */}
-    {/* <SignUp/> */}
-    Home Page
-  </div>;
+  return (
+    <div>
+      {/* <SignIn /> */}
+      <SignUp />
+      {/* Home Page */}
+    </div>
+  );
 }
 
 export default App;
