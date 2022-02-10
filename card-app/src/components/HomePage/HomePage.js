635bb23f0eba5bcc63d183aa57b4bd7e18b38165
captain-aman11 <kmraman11011@gmail.com>
2022-02-10 05:51:49 +0000

    fetch Data fix commit

--- card-app/src/components/HomePage/HomePage.js
+++ card-app/src/components/HomePage/HomePage.js
@@ -9,106 +9,108 @@ import {
   Button,
   Card,
 } from "react-bootstrap";
+import Grid from "@mui/material/Grid";
 
-function HomePage() {
-  const [isFlipped, setIsFlipped] = useState(false);
+export default function HomePage() {
+  {
+    const [isFlipped, setIsFlipped] = useState(false);
 
-  const [cardData, setCardData] = useState([]);
+    const [cardData, setCardData] = useState(null);
 
-  // useEffect function
-  useEffect(async () => {
-    const url = "https://rickandmortyapi.com/api/character";
+    // useEffect function
+    useEffect(() => {
+      //removed async here
+      const url = "https://rickandmortyapi.com/api/character";
 
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        const jsonData = json.results;
-        setCardData(jsonData);
-        console.log(jsonData);
-      } catch (error) {
-        console.log("error", error);
-      }
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          const jsonData = json.results;
+          setCardData(jsonData);
+          console.log(jsonData);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+      fetchData();
+    }, []);
+
+    const handleClick = () => {
+      setIsFlipped(!isFlipped);
     };
-    fetchData();
-  }, []);
 
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
-
-  return (
-    <>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Cards-App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#">Saved Cards</Nav.Link>
-              <Nav.Link href="#">Login</Nav.Link>
-              <Nav.Link href="#">Register</Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {cardData.map((c) => {
-        <h1>{c.id}</h1>;
-      })}
-      <Container style={{ height: "100vh" }}>
-        {cardData.map((c) => {
-          <h1>It's rendering here</h1>;
-          // <ReactCardFlip
-          //   key={c.id}
-          //   isFlipped={isFlipped}
-          //   flipDirection="vertical"
-          // >
-          //   <Card style={{ width: "300px" }}>
-          //     <Card.Img
-          //       variant="top"
-          //       src={c.image}
-          //       height="300px"
-          //       width="300px"
-          //     />
-          //     <Card.Body>
-          //       <Card.Title>{c.name}</Card.Title>
-          //       <Card.Text>{c.species}</Card.Text>
-          //       <Button variant="primary" onClick={handleClick}>
-          //         View Details
-          //       </Button>
-          //     </Card.Body>
-          //   </Card>
-
-          //   <Card style={{ width: "300px", height: "300px" }}>
-          //     <Card.Body>
-          //       <Card.Title>Character Details</Card.Title>
-          //       <Card.Text>Name : {c.name}</Card.Text>
-          //       <Card.Text>Status : {c.status}</Card.Text>
-          //       <Card.Text>Gender : {c.gender}</Card.Text>
-          //       <Card.Text>Species : {c.species}</Card.Text>
-          //       <Button variant="primary" onClick={handleClick}>
-          //         View Image
-          //       </Button>
-          //     </Card.Body>
-          //   </Card>
-          // </ReactCardFlip>;
-        })}
-      </Container>
-    </>
-  );
+    return (
+      <>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">Cards-App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#">Saved Cards</Nav.Link>
+                <Nav.Link href="#">Login</Nav.Link>
+                <Nav.Link href="#">Register</Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Grid container spacing={2}>
+          {cardData &&
+            cardData.map((c) => {
+              return (
+                <Grid item sx={{ ml: 1 }}>
+                  <ReactCardFlip
+                    key={c.id}
+                    isFlipped={isFlipped}
+                    flipDirection="vertical"
+                  >
+                    <Card>
+                      <Card.Img
+                        variant="top"
+                        src={c.image}
+                        height="300px"
+                        width="300px"
+                      />
+                      <Card.Body>
+                        <Card.Title>{c.name}</Card.Title>
+                        <Card.Text>{c.species}</Card.Text>
+                        <Button variant="primary" onClick={handleClick}>
+                          View Details
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                    <Card style={{ width: "300px", height: "300px" }}>
+                      <Card.Body>
+                        <Card.Title>Character Details</Card.Title>
+                        <Card.Text>Name : {c.name}</Card.Text>
+                        <Card.Text>Status : {c.status}</Card.Text>
+                        <Card.Text>Gender : {c.gender}</Card.Text>
+                        <Card.Text>Species : {c.species}</Card.Text>
+                        <Button variant="primary" onClick={handleClick}>
+                          View Image
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </ReactCardFlip>
+                </Grid>
+              );
+            })}
+        </Grid>
+      </>
+    );
+  }
 }
-
-export default HomePage;
